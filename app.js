da8cc335c6969bd615b69aa9c5dfd1269176b6d2
Ivan Camilo <rivancamilo@gmail.com>
2020-11-24 12:00:19 -0500

    actualizamos la interfaz de node

--- app.js
+++ app.js
@@ -39,7 +39,64 @@ mongoose
 aplicacion.get('/', async(req, res) => {
     const usuarios = await Usuarios.find()
     res.render('paginas/index', { usuarios });
-})
+});
+
+/**********************************************************
+    Registrar un Usuario desde la interfaz de nodejs
+**********************************************************/
+aplicacion.get("/nuevousuario", (req, res) => {
+    res.render('paginas/nuevoUser');
+});
+
+/*********************************************************
+ *  metodo para guardar el registro
+ *********************************************************/
+aplicacion.post("/procesarRegistro", async(req, res) => {
+
+    const usuario = new Usuarios(req.body);
+    await usuario.save();
+    res.redirect('/');
+});
+
+/*********************************************************
+    Actualizar un usuario
+**********************************************************/
+
+aplicacion.get('/editar/:id', async(req, res) => {
+    const { id } = req.params; //obtenemos el id del usuario
+    const datosUsuario = await Usuarios.findOne({ _id: id });
+    res.render('paginas/editarUser', { usuario: datosUsuario });
+
+});
+
+
+aplicacion.post('/editar/:id', async(req, res) => {
+    const { id } = req.params; //obtenemos el id del usuario
+    const datosFinales = req.body;
+
+    await Usuarios.update({ _id: id }, datosFinales);
+    res.redirect('/');
+
+});
+
+/*********************************************************
+    Eliminar un usuario
+*********************************************************/
+aplicacion.get("/borrar/:id", async(req, res) => {
+
+    const { id } = req.params; //obtenemos el id del registro
+    await Usuarios.remove({ _id: id }); //eliminamos el registro
+    res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
 
 
 /**********************************************************
@@ -48,34 +105,12 @@ Listar todos los usuarios
 aplicacion.get("/api/usuarios", async(req, res) => {
     const lisUsuarios = await Usuarios.find();
     const paises = await Paises.find();
-    //if (lisUsuarios.length > 0) {
 
-    //res.json({ data: lisUsuarios });
     return res.status(200).send({
         status: "success",
         usuarios: lisUsuarios,
         paises: paises,
     });
-    /* 
-    } else {
-        res.json({ errors: ["No se encontro Informacion"] });
-        res.status(404);
-    } */
-});
-
-/**********************************************************
-    Registrar un Usuario desde la interfaz de nodejs
-**********************************************************/
-
-aplicacion.get("/nuevousuario", (req, res) => {
-    res.render('paginas/nuevoUser');
-});
-
-aplicacion.post("/procesarRegistro", async(req, res) => {
-
-    const usuario = new Usuarios(req.body);
-    await usuario.save();
-    res.redirect('/');
 });
 
 /**********************************************************
@@ -94,6 +129,26 @@ aplicacion.post("/api/nuevousuario", async(req, res) => {
 /*********************************************************
     Actualizar un usuario
 **********************************************************/
+aplicacion.get("/api/editarusuario/:id", async(req, res) => {
+    const { id } = req.params; //obtenemos el id del usuario
+
+    const datosUsuario = await Usuarios.findOne({ _id: id });
+    if (!datosUsuario) {
+        return res.status(204).json({
+            status: "Error",
+            usurio: "Usuario no encontrado",
+        });
+    } else {
+        return res.status(200).json({
+            status: "success",
+            usurio: datosUsuario,
+        });
+    }
+
+});
+
+
+
 aplicacion.post("/api/editarusuario/:id", async(req, res) => {
     const { id } = req.params; //obtenemos el id del usuario
     const datosFinales = req.body;
@@ -108,13 +163,12 @@ aplicacion.post("/api/editarusuario/:id", async(req, res) => {
 /*********************************************************
     Eliminar un usuario
 *********************************************************/
-aplicacion.get("/api/borrarusuario/:id", async(req, res) => {
-    const { id } = req.params; //obtenemos el id del registro
-    /* const usuario = await Usuarios.findById(id); //buscar el usuario
-    const datosFinales = req.body; */
+aplicacion.delete("/api/borrarusuario/:id", async(req, res) => {
 
+    const { id } = req.params; //obtenemos el id del registro
     await Usuarios.remove({ _id: id }); //eliminamos el registro
     return res.status(200);
+
 });
 
 /*********************************************************
