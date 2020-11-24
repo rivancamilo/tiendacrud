7f77b2ae05903ad17d5f7002ecf69df6e20fb40f
Ivan Camilo <rivancamilo@gmail.com>
2020-11-24 14:27:37 -0500

    cambios para la actualizacion del usuario

--- app.js
+++ app.js
@@ -36,20 +36,24 @@ mongoose
     .catch((err) => console.log(err));
 
 
+
+/***********************************************************
+    Listamos todos los usuarios
+***********************************************************/
 aplicacion.get('/', async(req, res) => {
     const usuarios = await Usuarios.find()
     res.render('paginas/index', { usuarios });
 });
 
 /**********************************************************
-    Registrar un Usuario desde la interfaz de nodejs
+    Rednderiza el formulario para crear nuevo Usuario
 **********************************************************/
 aplicacion.get("/nuevousuario", (req, res) => {
     res.render('paginas/nuevoUser');
 });
 
 /*********************************************************
- *  metodo para guardar el registro
+    Creamos el usuario en la BD 
  *********************************************************/
 aplicacion.post("/procesarRegistro", async(req, res) => {
 
@@ -59,9 +63,8 @@ aplicacion.post("/procesarRegistro", async(req, res) => {
 });
 
 /*********************************************************
-    Actualizar un usuario
+    Renderizamos el formulario con los datos del Usuario
 **********************************************************/
-
 aplicacion.get('/editar/:id', async(req, res) => {
     const { id } = req.params; //obtenemos el id del usuario
     const datosUsuario = await Usuarios.findOne({ _id: id });
@@ -69,21 +72,20 @@ aplicacion.get('/editar/:id', async(req, res) => {
 
 });
 
-
+/**********************************************************
+    Actualizamos los datos del formulario
+**********************************************************/
 aplicacion.post('/editar/:id', async(req, res) => {
     const { id } = req.params; //obtenemos el id del usuario
     const datosFinales = req.body;
-
     await Usuarios.update({ _id: id }, datosFinales);
     res.redirect('/');
-
 });
 
 /*********************************************************
     Eliminar un usuario
 *********************************************************/
 aplicacion.get("/borrar/:id", async(req, res) => {
-
     const { id } = req.params; //obtenemos el id del registro
     await Usuarios.remove({ _id: id }); //eliminamos el registro
     res.redirect('/');
@@ -100,7 +102,7 @@ aplicacion.get("/borrar/:id", async(req, res) => {
 
 
 /**********************************************************
-Listar todos los usuarios
+Listar todos los usuarios EN EL API
 **********************************************************/
 aplicacion.get("/api/usuarios", async(req, res) => {
     const lisUsuarios = await Usuarios.find();
@@ -114,20 +116,36 @@ aplicacion.get("/api/usuarios", async(req, res) => {
 });
 
 /**********************************************************
-    Registrar un Usuario desde la interfaz de Angular
+    Registrar un Usuario desde la API
 **********************************************************/
 aplicacion.post("/api/nuevousuario", async(req, res) => {
     const usuario = new Usuarios(req.body);
-    await usuario.save();
+    await usuario.save((err, usuario) => {
 
-    return res.status(200).send({
-        status: "success",
-        usuario,
+        if (err || !usuario) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'El usuario no se ha guardado !!!'
+            });
+        }
+
+        // Devolver una respuesta 
+        return res.status(200).send({
+            status: 'success',
+            usuario
+        });
+
     });
+
+    /*return res.status(200).send({
+    status: "success",
+    usuario,
+    }); */
 });
 
+
 /*********************************************************
-    Actualizar un usuario
+    Muestra los datos actuales del Usuario en el API
 **********************************************************/
 aplicacion.get("/api/editarusuario/:id", async(req, res) => {
     const { id } = req.params; //obtenemos el id del usuario
@@ -136,12 +154,12 @@ aplicacion.get("/api/editarusuario/:id", async(req, res) => {
     if (!datosUsuario) {
         return res.status(204).json({
             status: "Error",
-            usurio: "Usuario no encontrado",
+            usuario: "Usuario no encontrado",
         });
     } else {
         return res.status(200).json({
             status: "success",
-            usurio: datosUsuario,
+            usuario: datosUsuario,
         });
     }
 
@@ -151,19 +169,17 @@ aplicacion.get("/api/editarusuario/:id", async(req, res) => {
 
 aplicacion.post("/api/editarusuario/:id", async(req, res) => {
     const { id } = req.params; //obtenemos el id del usuario
-    const datosFinales = req.body;
 
-    await Usuarios.update({ _id: id }, datosFinales);
+    await Usuarios.update({ _id: id }, req.body);
     return res.status(200).json({
-        status: "success",
-        usurio: datosFinales,
+        status: "success"
     });
 });
 
 /*********************************************************
     Eliminar un usuario
 *********************************************************/
-aplicacion.delete("/api/borrarusuario/:id", async(req, res) => {
+aplicacion.get("/api/borrarusuario/:id", async(req, res) => {
 
     const { id } = req.params; //obtenemos el id del registro
     await Usuarios.remove({ _id: id }); //eliminamos el registro
